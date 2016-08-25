cb60b6b0e3f77015142d8c3a828196d1c169aacd
aduriseti <aduriseti@gmail.com>
2016-08-25 14:48:26 -0700

    packaged

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='2factorauth_filesync',
 	version='1.0',
 	description='A file sync utility that mimics the functionality of WinSCP for Unix based platforms and only requires approval once from 2 stage authentication',
-	url='git@github.com:aduriseti/FileSync_2_stage_authentication.git',
+	url='https://github.com/aduriseti/FileSync_2_stage_authentication.git',
 	author='Amal Duriseti',
 	author_email='aduriseti@gmail.com',
 	license='MIT',
